ddc5c996729bfe6868e2b241d24d78ba62b39b1a
karimi <zmanyamyd5@gmail.com>
2022-04-19 06:37:05 +0430

    init WifiManager

--- app/src/main/java/com/example/wifip2p_connection/MainActivity.java
+++ app/src/main/java/com/example/wifip2p_connection/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingComponent;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -15,7 +18,9 @@ import com.example.wifip2p_connection.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
-
+    private WifiManager wifiManager;
+    private WifiP2pManager mManager;
+    private WifiP2pManager.Channel mChannel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +30,13 @@ public class MainActivity extends AppCompatActivity {
         initDataBinding();
     }
 
-
-
-
     private void initDataBinding(){
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+
+    private void initWifiManager(){
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
     }
 }
