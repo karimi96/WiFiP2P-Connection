3e0224f04a84236e1ba1de01062d27fdaba23e5b
karimi <zmanyamyd5@gmail.com>
2022-04-19 06:32:49 +0430

    add DataBinding

--- app/src/main/java/com/example/wifip2p_connection/MainActivity.java
+++ app/src/main/java/com/example/wifip2p_connection/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.wifip2p_connection;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingComponent;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.example.wifip2p_connection.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initDataBinding();
+    }
+
+
+
+
+    private void initDataBinding(){
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
